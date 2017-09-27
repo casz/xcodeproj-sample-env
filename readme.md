8bedc57f0539dacca51d3cbd5b87199152ae61d0
Joseph Petersen <josephp90@gmail.com>
2017-09-27 18:32:41 +0200

    Initial sample

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Run
+
+`fastlane ios build override:true`
+or
+`APP_IDENTIFIER=com.widex.appstore.xcodeproj-sample-env fastlane ios build`
---
+++ xcodeproj-sample-env/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xcodeproj-sample-env
+//
+//  Created by CPASBUILDER on 27/09/2017.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
