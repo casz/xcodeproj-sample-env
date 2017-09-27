6467d08afde680279aead8151958c58bdc87790c
Joseph Petersen <josephp90@gmail.com>
2017-09-27 19:54:40 +0200

    spec it further up

--- env_spec.rb
+++ env_spec.rb
@@ -1,11 +1,38 @@
 require 'xcodeproj'
 
-ENV['APP_IDENTIFIER'] = 'com.widex.appstore.xcodeproj-sample-env'
 ENV['RICK'] = 'WHAT UP MY GLIP GLOPS!'
 ENV['MORTY'] = 'Dont be trippin dog we got you'
+normal = 'com.widex.enterprise.xcodeproj-sample-env'
 
 describe 'xcodeproj variables' do
-  it 'substitues variables' do
+
+  it 'should still resolve xcode build settings' do
+    project = Xcodeproj::Project.open('xcodeproj-sample-env.xcodeproj')
+    project.targets.each do |target|
+      target.build_configuration_list.build_configurations.each do |build_configuration|
+        app_identifier = build_configuration.resolve_build_setting('APP_IDENTIFIER')
+        bundle_identifier = build_configuration.resolve_build_setting('PRODUCT_BUNDLE_IDENTIFIER')
+        expect(app_identifier).to eq(normal)
+      end
+    end
+  end
+
+  it 'subtitues variables that are resolved by xcode build settings' do
+    project = Xcodeproj::Project.open('xcodeproj-sample-env.xcodeproj')
+    project.targets.each do |target|
+      target.build_configuration_list.build_configurations.each do |build_configuration|
+        app_identifier = build_configuration.resolve_build_setting('APP_IDENTIFIER')
+        bundle_identifier = build_configuration.resolve_build_setting('PRODUCT_BUNDLE_IDENTIFIER')
+        expect(app_identifier).to eq(normal)
+        expect(bundle_identifier).to eq(normal) if target.name == 'xcodeproj-sample-env'
+        expect(bundle_identifier).to eq("#{normal}.watchkitapp") if target.name == 'xcodeproj-sample-env-watch Extension'
+        expect(bundle_identifier).to eq("#{normal}.watchkitapp.watchkitextension") if target.name == 'xcodeproj-sample-env-watch'
+      end
+    end
+  end
+
+  it 'substitues bundle identifiers variable' do
+    ENV['APP_IDENTIFIER'] = 'com.widex.appstore.xcodeproj-sample-env'
     project = Xcodeproj::Project.open('xcodeproj-sample-env.xcodeproj')
     project.targets.each do |target|
       target.build_configuration_list.build_configurations.each do |build_configuration|
@@ -19,7 +46,7 @@ describe 'xcodeproj variables' do
     end
   end
 
-  it 'substitues debug env variables' do
+  it 'substitues debug env variable' do
     project = Xcodeproj::Project.open('xcodeproj-sample-env.xcodeproj')
     project.targets.each do |target|
       target.build_configuration_list.build_configurations.each do |build_configuration|
@@ -29,7 +56,7 @@ describe 'xcodeproj variables' do
     end
   end
 
-  it 'substitues release env variables' do
+  it 'substitues release env variable' do
     project = Xcodeproj::Project.open('xcodeproj-sample-env.xcodeproj')
     project.targets.each do |target|
       target.build_configuration_list.build_configurations.each do |build_configuration|
